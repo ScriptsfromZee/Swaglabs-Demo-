fe525d1620b001945122efba3842fa7e6978ccf3
zogam <derekchizogam@gmail.com>
2024-10-10 08:49:03 +0100

    Updated code  with comments

--- cypress/e2e/SwagLabs Demo.cy.js
+++ cypress/e2e/SwagLabs Demo.cy.js
@@ -1,81 +1,73 @@
 describe("Login Test Suite", () => {
 
-  it ("Swaglabs Login using Xpath", () => {
-    cy. log("Test 1 has started");
+  it("Swaglabs Login using Xpath", () => {
+    cy.log("Test 1 has started");
 
-    cy. visit ("https://www.saucedemo.com/v1/");
+    // Navigate to the Swag Labs URL
+    cy.visit("https://www.saucedemo.com/v1/");
+    cy.log("Navigated to URL");
 
-    cy. log("Navigated to URL")
+    // Fill in the username
+    cy.xpath("//input[@id='user-name']").type("standard_user");
+    cy.log("Username has been filled");
 
-    cy.xpath("//input [@id= 'user-name' ]").type("standard_user")
+    // Fill in the password
+    cy.xpath("//input[@id='password']").type('secret_sauce');
 
-    cy. log("Username has been filled")
-
-    cy.xpath ("//input [@id= 'password']").type('secret_sauce')
-
-    cy.xpath("//input [@id='login-button']"). click()
-
-    cy. log("User has signed in succesfully");
+    // Click the login button
+    cy.xpath("//input[@id='login-button']").click();
+    cy.log("User has signed in successfully");
   });
-
 
   it("Swaglabs Login using CSS Selectors", () => {
     cy.log("Test 2 has started");
 
+    // Navigate to the Swag Labs URL
     cy.visit("https://www.saucedemo.com/v1/");
-
     cy.log("Navigated to URL");
 
+    // Fill in username and password using CSS selectors
     cy.get('input[type="text"]').type("standard_user");
-
     cy.get('input[type="password"]').type("secret_sauce");
 
+    // Click the login button
     cy.get('input[type="submit"]').click();
+    cy.log("User has signed in successfully");
 
-    cy.log("User has signed in succesfully");
-
+    // Add items to cart
     cy.get('button[class="btn_primary btn_inventory"]').eq(0).click();
+    
+    // Ensure the second item is visible before clicking
+    cy.get('button[class="btn_primary btn_inventory"]').eq(1).should('be.visible').click();
 
-    cy.wait(2000);
-
-    cy.get('button[class="btn_primary btn_inventory"]').eq(1).click();
-
-    cy.wait(2000);
-
+    // Scroll to and verify item visibility
     cy.contains("Test.allTheThings").scrollIntoView().should("be.visible");
 
-    cy.wait(2000);
+    // Add another item to cart
+    cy.get("#inventory_container > div > div:nth-child(6) > div.pricebar > button").should('be.visible').click();
+    
+    cy.log("User has successfully filled cart");
 
-    cy.get("#inventory_container > div > div:nth-child(6) > div.pricebar > button").click();
+    // Click on the cart icon after ensuring it's visible
+    cy.get('svg[aria-hidden="true"]').should('be.visible').click();
 
-    cy.log("User has succesfully filled cart");
+    // Proceed to checkout after ensuring the button is visible
+    cy.get('a[class="btn_action checkout_button"]').should('be.visible').click();
 
-    cy.wait(1000);
-
-    cy.get('svg[aria-hidden="true"]').click();
-
-    cy.wait(1000);
-
-    cy.get('a[class="btn_action checkout_button"]').click();
-
+    // Fill out checkout information
     cy.get('input[id="first-name"]').type("David");
+    
+    // Fill out last name and postal code after ensuring visibility
+    cy.get('input[id="last-name"]').should('be.visible').type("Jones");
+    
+    cy.get('input[id="postal-code"]').should('be.visible').type("500101");
 
-    cy.wait(1500);
+   // Submit the checkout form
+   cy.get('input[type="submit"]').click();
 
-    cy.get('input[id="last-name"]').type("Jones");
+   // Return to cart after ensuring visibility
+   cy.get('a[class="btn_action cart_button"]').should('be.visible').click();
 
-    cy.wait(1500);
-
-    cy.get('input[id="postal-code"]').type("500101");
-
-    cy.get('input[type="submit"]').click();
-
-    cy.wait(1200);
-
-    cy.get('a[class="btn_action cart_button"]').click();
-
-    cy.log("User has succesfully placed an order");
+   cy.log("User has successfully placed an order");
   });  
-});       
-     
-
+});
